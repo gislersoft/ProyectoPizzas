0a72cdace315ed9cb330251cc787d7f7a186f340
Ivan Viveros <ivan.viveros@correounivalle.edu.co>
2019-09-26 17:58:10 -0500

    Base

---
+++ SuperPizzas/SuperPizzas/utils.py
@@ -0,0 +1,80 @@
+from dotenv import dotenv_values
+from typing import Union
+
+
+EnvSettingTypes = (str, bool, int, float, list)
+EnvSetting = Union[str, bool, int, float, list]
+
+
+def get_env_value(env: dict, key: str, default: EnvSetting = None):
+    """get & cast a given value from a dictionary, or return the default"""
+    if key in env:
+        value = env[key]
+    else:
+        return default
+
+    ExpectedType = type(default)
+    assert (
+        ExpectedType in EnvSettingTypes
+    ), f"Tried to set unsupported environemnt variable {key} to {ExpectedType}"
+
+    def raise_typerror():
+        raise TypeError(
+            f"Got bad environment variable {key}={value}" f" (expected {ExpectedType})"
+        )
+
+    if ExpectedType is str:
+        return value
+    elif ExpectedType is bool:
+        if value.lower() == "true":
+            return True
+        elif value.lower() == "false":
+            return False
+        else:
+            raise_typerror()
+    elif ExpectedType is int:
+        if value.isdigit():
+            return int(value)
+        else:
+            raise_typerror()
+    elif ExpectedType is float:
+        try:
+            return float(value)
+        except ValueError:
+            raise_typerror()
+    elif ExpectedType is list:
+        return value.split(",")
+
+
+def unique_env_settings(env: dict, defaults: dict) -> dict:
+    """return all the new valid env settings in a dictionary of settings"""
+    existing_settings = {
+        setting_name: val
+        for setting_name, val in (defaults or env).items()
+        if not setting_name.startswith("_") and setting_name.isupper()
+    }
+    if not defaults:
+        return existing_settings
+
+    new_settings = {}
+    for setting_name, default_val in existing_settings.items():
+        loaded_val = get_env_value(env, setting_name, default_val)
+
+        if loaded_val != default_val:
+            new_settings[setting_name] = loaded_val
+
+    return new_settings
+
+
+def load_env_settings(
+    dotenv_path: str = None, env: dict = None, defaults: dict = None
+) -> dict:
+    """load settings from a dotenv file or os.environ by default"""
+    assert not (dotenv_path and env), "Only pass env or dotenv_path, not both"
+
+    env_values = (env or {}).copy()
+    defaults = (defaults or {}).copy()
+    if dotenv_path:
+        env_values = dotenv_values(dotenv_path=dotenv_path)
+
+    return unique_env_settings(env_values, defaults)
---
+++ SuperPizzas/users/models.py
@@ -0,0 +1,46 @@
+import os
+
+from django.conf import settings
+from django.core.exceptions import ValidationError
+from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.utils import timezone
+from django.utils.functional import cached_property
+
+
+def avatar_path(instance, filename):
+    _, file_extension = os.path.splitext(filename)
+
+    return "/".join(["users", str(instance.id), f"avatar{file_extension}"])
+
+
+class User(AbstractBaseUser):
+    ADMINISTRATOR = "Administrador"
+    FRANCHISE = "Franquicia"
+    DEFAULT_AVATAR = "images/profile.png"
+
+    USER_TYPES = ((ADMINISTRATOR,ADMINISTRATOR),(FRANCHISE, FRANCHISE))
+
+    email = models.EmailField("Correo Electrónico", blank=True, unique=True)
+    first_name = models.CharField("Nombres", max_length=150, blank=True)
+    last_name = models.CharField("Apellidos", max_length=150, blank=True)
+
+    is_staff = models.BooleanField("Staff?", default=False)
+    is_active = models.BooleanField("Activo?", default=True)
+    date_joined = models.DateTimeField("Fecha de Registro", default=timezone.now)
+    avatar = models.ImageField(
+        verbose_name="Imagen", upload_to=avatar_path, blank=True, null=True
+    )
+    user_type = models.CharField(
+        "Tipo de Usuario", max_length=50, choices=USER_TYPES, null=True, blank=True
+    )
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    @property
+    def avatar_url(self):
+        if self.avatar:
+            return self.avatar.url
+        return f"{settings.STATIC_URL}{User.DEFAULT_AVATAR}"
