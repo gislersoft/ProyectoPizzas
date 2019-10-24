788d89b072b19b52bd856cde0b88bb92aae807ba
SH-Arias <hernandez.santiago@correounivalle.edu.co>
2019-10-23 23:29:09 -0500

    HU#22 (cambio 1)

--- users/forms.py
+++ users/forms.py
@@ -5,9 +5,11 @@ from .models import User
 
 
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=100, help_text='Required. 100 charaters of fewer.')
-    last_name = forms.CharField(max_length=100, help_text='Required. 100 charaters of fewer.')
-    email = forms.CharField(max_length=100, help_text='Required. 100 charaters of fewer.')
+    first_name = forms.CharField(max_length=100, help_text='Necesario. 100 caracteres o menos.', label="Nombre")
+    last_name = forms.CharField(max_length=100, help_text='Necesario. 100 caracteres o menos.', label="Apellido")
+    email = forms.CharField(max_length=100, help_text='Necesario. 100 caracteres o menos.')
+    password2 = forms.CharField(help_text='Introduce la misma contraseña para verificación.', label="Confirmar contraseña", widget=forms.PasswordInput())
+    password1 = forms.CharField(help_text='Introduce tu contraseña.', label="Contraseña", widget=forms.PasswordInput())
 
     class Meta:
         model = User
